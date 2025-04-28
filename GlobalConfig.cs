aa9cf0425c55d364cbbeb239dfd0e77861d33dc3
janSlobo <113860499+janSlobo@users.noreply.github.com>
2025-04-28 17:58:48 +0200

    Update GlobalConfig.cs

--- GlobalConfig.cs
+++ GlobalConfig.cs
@@ -2,7 +2,7 @@
 {
     public class GlobalConfig
     {
-        public static string connstring = "Data Source=SQL6030.site4now.net;Initial Catalog=db_ab77de_statements;User Id=db_ab77de_statements_admin;Password=Vratimov145";
+        public static string connstring = "Data Source=SQL6030.site4now.net;Initial Catalog=db_ab77de_statements;User Id=db_ab77de_statements_admin;Password=";
 
     }
 }
